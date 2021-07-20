441cb3a60427c81939edc4ccfb2da94c78385ec9
Erick Santos <71516100+Erick88santos@users.noreply.github.com>
2021-07-20 14:43:25 -0300

    Atividade web1
    MultasCalc

---
+++ test.js
@@ -0,0 +1,8 @@
+var  vel = 50
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+console.log(`Considerando que a velocdade Máxima da via é 60km/h`)
+if (vel > 60) { //condição simples
+    console.log(`Você será MULTADO por excesso de velocidade`)
+
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
